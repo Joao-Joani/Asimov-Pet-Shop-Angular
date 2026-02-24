0e260fc15e837a47008254f51fba25393bf8b93a
Joao Joani <jgabriel.joani@gmail.com>
2026-02-24 17:55:40 -0300

    Integração Firebase.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,19 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { AngularFireModule } from '@angular/fire/compat';
+import { environment } from '../environments/environment';
+import { AngularFireAuthModule } from '@angular/fire/compat/auth';
+
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    AngularFireModule.initializeApp(environment.firebase),
+    AngularFireAuthModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/environments/environment.ts
@@ -0,0 +1,12 @@
+export const environment = {
+    production : false,
+    firebase : {
+        apiKey: "AIzaSyC-KvYUxM7mU4QL6h2Wr-ndDwuizu4hcrw",
+        authDomain: "petshop-62522.firebaseapp.com",
+        projectId: "petshop-62522",
+        storageBucket: "petshop-62522.firebasestorage.app",
+        messagingSenderId: "54743071607",
+        appId: "1:54743071607:web:7b3d1fe15e01d67c5c7f72",
+        measurementId: "G-8W0PHTJEZ8"
+    }
+};
