64fe3aa34c55628737220ec438b2ceb51f1fbb58
Joao Joani <jgabriel.joani@gmail.com>
2026-02-26 16:55:38 -0300

    Criação dos componentes Home, Header e Footer.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,13 +11,19 @@ import { AngularFireAuthModule } from '@angular/fire/compat/auth';
 import { LoginComponent } from './components/login/login.component';
 import { CadastroComponent } from './components/cadastro/cadastro.component';
 import { RecuperarSenhaComponent } from './components/recuperar-senha/recuperar-senha.component';
+import { HomeComponent } from './components/home/home.component';
+import { FooterComponent } from './shared/components/footer/footer.component';
+import { HeaderComponent } from './shared/components/header/header.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
     CadastroComponent,
-    RecuperarSenhaComponent
+    RecuperarSenhaComponent,
+    HomeComponent,
+    FooterComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
