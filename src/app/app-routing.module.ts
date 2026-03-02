7f7dca7f4247d7df0b7a1eb7db362c9cb6ae165e
Joao Joani <jgabriel.joani@gmail.com>
2026-03-02 19:41:12 -0300

    Estruturação do template das telas do site.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,7 +2,6 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './components/login/login.component';
 import { CadastroComponent } from './components/cadastro/cadastro.component';
-import { RecuperarSenhaComponent } from './components/recuperar-senha/recuperar-senha.component';
 import { HomeComponent } from './components/home/home.component';
 import { InicioComponent } from './components/inicio/inicio.component';
 import { PerfilComponent } from './components/perfil/perfil.component';
